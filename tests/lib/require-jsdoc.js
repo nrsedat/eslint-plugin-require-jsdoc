b9e0d957da6357a1f3001e7b734724005c44563a
Naithalath, Ratheesh <Ratheesh_Naithalath@condenast.com>
2015-04-21 00:50:47 -0400

    Adding more tests

--- tests/lib/require-jsdoc.js
+++ tests/lib/require-jsdoc.js
@@ -6,13 +6,60 @@ var eslintTester = new ESLintTester(linter);
 
 eslintTester.addRuleTest('./lib/rules/require-jsdoc', {
     valid: [
-        '/*@return*/function doSomething() {return 1+2;}'
+        '/*@return*/function doSomething() {return 1+2;}',
+        '/*@param @return*/function doSomething(string) {return string;}',
+        '/*@param @return*/var test = function(string){console.log("test");}',
+        'var comp = React.createClass({/*@return*/render: function(){}});'
     ],
 
     invalid: [
         {
+            code: 'var outerFunction = function(){/*@return*/function innerFunction(){}}',
+            errors: [ { message: 'Missing @jsdoc for outerFunction' }]
+        },
+        {
             code: 'function doSomething() {return 1+2;}',
             errors: [ { message: 'Missing @jsdoc for doSomething' } ]
+        },
+        {
+            code: 'function doSomething(string) {return string}',
+            errors: [ { message: 'Missing @jsdoc for doSomething' } ]
+        },
+        {
+            code: '/*@return*/function doSomething(string) {return string}',
+            errors: [ { message: 'Missing @param definitions' } ]
+        },
+        {
+            code: '/*@param*/function doSomething(string) {return string}',
+            errors: [ { message: 'Missing @return statement in jsdoc' } ]
+        },
+        {
+            code: 'var test = function(){console.log("test");}',
+            errors: [ { message: 'Missing @jsdoc for test' } ]
+        },
+        {
+            code: 'var test = function(string){console.log("test");}',
+            errors: [ { message: 'Missing @jsdoc for test' } ]
+        },
+        {
+            code: '/*@return*/var test = function(string){console.log(string);}',
+            errors: [ { message: 'Missing @param definitions' } ]
+        },
+        {
+            code: '/*@param*/var test = function(string){console.log(string);}',
+            errors: [ { message: 'Missing @return statement' } ]
+        },
+        {
+            code: '/*@param*/var test = function(string,string2){console.log(string);}',
+            errors: [ {message: 'Missing @return statement'}, { message: 'Missing @param definitions' } ]
+        },
+        {
+            code: '/*@param@return*/var test = function(var1, var2){console.log(string);}',
+            errors: [ { message: 'Missing @param definitions' } ]
+        },
+        {
+            code: 'var comp = React.createClass({render: function(){}});',
+            errors: [ { message: 'Missing @jsdoc for render' } ]
         }
     ]
 });
